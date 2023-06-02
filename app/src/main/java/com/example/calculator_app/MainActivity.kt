ae51785e3cd35aa9eb6ba53c6ddd9a05c910f86a
Eugene Choi <choi.eugene.97@gmail.com>
2023-06-02 00:24:08 -0700

    feat: display numbers in TextView on click
    The TextView with @id/working now displays the number associated with the button view that was clicked. If the button clicked is a number, it will append the CharSequence contained in @id/working. Otherwise, it will not append.

--- app/src/main/java/com/example/calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/calculator_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculator_app
 import android.os.Bundle
 import android.text.TextUtils.substring
 import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.isDigitsOnly
 import com.example.calculator_app.ui.theme.Calculator_AppTheme
 import com.example.calculator_app.databinding.CalculatorBinding
 
@@ -27,20 +29,34 @@ class MainActivity : ComponentActivity() {
     // Names for the following two functions cannot change since they are specifically called on by
 // the
     fun numberAction(view: View) {
+        if (view is Button) {
+            val num = view.text.isDigitsOnly()
+            if (num) {
+                binding.working.append(view.text)
+            }
+            else{
+                //filler
+            }
+        }
 
     }
 
     fun operationAction(view: View) {
 
     }
+
     // Clear the data in the results and working TextViews
-    fun clear(view: View){
+    fun clear(view: View) {
         binding.results.text = ""
         binding.working.text = ""
     }
 
-    fun delete(view: View){
+    // Erase one char from the charSequence in @id/display
+    fun delete(view: View) {
         val displayChars: CharSequence = binding.working.text
-        if(displayChars.isNotEmpty()) binding.working.text = displayChars.substring(0, displayChars.length-1)
+        if (displayChars.isNotEmpty()) binding.working.text =
+            displayChars.substring(0, displayChars.length - 1)
     }
+
+
 }
