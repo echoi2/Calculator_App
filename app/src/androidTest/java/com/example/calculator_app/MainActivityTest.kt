ff22a9213112c34479634568e1cf05a495944473
Eugene Choi <choi.eugene.97@gmail.com>
2023-06-02 00:31:52 -0700

    Add Kotlin file named MainActivityTest for InstrumentedTest

--- app/src/androidTest/java/com/example/calculator_app/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/calculator_app/MainActivityTest.kt
@@ -1,5 +1,28 @@
 package com.example.calculator_app
 
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivityTest
+class MainActivityTest{
+
+    @get:Rule
+    val rule = createComposeRule()
+    val buttonDel = hasText("⌫") and hasClickAction()
+
+    @Test
+    fun clickDelete_deletesChar(){
+        rule.setContent { MainActivity() }
+
+        rule.onNode(buttonDel).performClick()
+
+
+    }
+}
