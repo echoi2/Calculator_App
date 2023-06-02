9c5a016eda9c6f687d1f477e4764f7ee1a56fece
Eugene Choi <choi.eugene.97@gmail.com>
2023-06-02 15:44:30 -0700

    feat: have math operation signs display @id/working TextView
    Specific ways that the operations display listed below
    - TextView can't start with an operation sign unless it is a minus
      sign
    - TextView can't have multiple operation signs next to each other
    - Minus is the only TextView sign that can come after another
      operation sign other than itself
    - TextView can have operation signs right after decimal point
    - If the char before a minus sign is another operation sign and if you
      click another opertion sign after that minus sign, then you just
      delete that minus sign

--- app/src/main/java/com/example/calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/calculator_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.TextUtils.substring
 import android.view.View
@@ -17,6 +18,20 @@ import androidx.core.text.isDigitsOnly
 import com.example.calculator_app.ui.theme.Calculator_AppTheme
 import com.example.calculator_app.databinding.CalculatorBinding
 
+
+/*
+RULES:
+
+1. Display can't start with an operation sign unless it is a minus sign
+2. Display can't have multiple operation signs next to each other
+3. Minus is the only operation sign that can come after another operation sign other than itself
+3. Display can have operation signs right after decimal point
+4. If the char before a minus sign is another operation sign and if you click another opertion
+   sign after that minus sign, then you just delete that minus sign
+ */
+
+
+
 class MainActivity : ComponentActivity() {
     private lateinit var binding: CalculatorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +43,8 @@ class MainActivity : ComponentActivity() {
 
     // Names for the following two functions cannot change since they are specifically called on by
 // the
+    // As it is right now, even if the amount of numbers being added exceeds the size of the TextView,
+    // they are still being appended onto the text of the TextView (just not being shown)
     fun numberAction(view: View) {
         if (view is Button) {
             val num = view.text.isDigitsOnly()
@@ -41,8 +58,36 @@ class MainActivity : ComponentActivity() {
 
     }
 
+    @SuppressLint("SetTextI18n", "SuspiciousIndentation")
     fun operationAction(view: View) {
-
+        val workingText: CharSequence = binding.working.text
+        if(view is Button){
+            val opSign = view.text
+            val workView = binding.working
+            if(workingText.isNotEmpty()){
+                if(workingText.last().isDigit()){
+                    workView.append(opSign)
+                }
+                else{
+                    if(workingText.last() == '-' && !workingText[workingText.lastIndex-1].isDigit()){
+                        if(opSign != "-")
+                        binding.working.text = workingText.substring(0, workingText.length - 1)
+                    }
+                    else if(opSign == "-"){
+                        if(workingText.last() != '-')
+                            workView.append(opSign)
+                    }
+                    else{
+                        workView.text = workView.text.replaceRange(
+                            workView.text.lastIndex,
+                            workView.text.lastIndex + 1, opSign)
+                    }
+                }
+            }
+            else{
+                if (opSign == "-") workView.append(opSign)
+            }
+        }
     }
 
     // Clear the data in the results and working TextViews
